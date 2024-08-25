abb0ee3ed9fb471bce73c7fe9afe4bd729776556
MZeynelozkan <zeyzey4340@gmail.com>
2024-08-25 17:46:10 +0300

    first commit

--- src/features/blogtexts/BlogTable.jsx
+++ src/features/blogtexts/BlogTable.jsx
@@ -20,7 +20,7 @@ function BlogTable() {
   }
 
   return (
-    <div>
+    <div className="container mx-auto p-5">
       <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
         {blogData?.map((blog) => (
           <CardDefault key={blog.id} blog={blog} />
